b4b8d81f8803f666d958cb80fab2d999d901d547
Srilalitha S <srilalit@thoughtworks.com>
2019-12-09 00:35:52 +0530

    [Day-2][Part-b] generate the noun and verb for the intCode

--- src/macosMain/kotlin/sample/IntCodeProgram.kt
+++ src/macosMain/kotlin/sample/IntCodeProgram.kt
@@ -26,8 +26,42 @@ fun doOperation(codes: Array<Int>): Array<Int> {
     return codes
 }
 
-fun main() {
-    val miniCodes = arrayOf(1,12,2,3,1,1,2,3,1,3,4,3,1
+fun findNounAndVerb() {
+    var noun = 0
+    var verb = 0
+    loop@ do {
+        val miniCodes = arrayOf(1,noun,verb,3,1,1,2,3,1,3,4,3,1
+            ,5,0,3,2,6,1,19,1,5,19,23,1,13,23,27,
+            1,6,27,31,2,31,13,35,1,9,35,39,2,39,13
+            ,43,1,43,10,47,1,47,13,51,2,13,51,55,1,55
+            ,9,59,1,59,5,63,1,6,63,67,1,13,67,71,2,71,
+            10,75,1,6,75,79,1,79,10,83,1,5,83,87,2,10,
+            87,91,1,6,91,95,1,9,95,99,1,99,9,103,2,103,
+            10,107,1,5,107,111,1,9,111,115,2,13,115,119,
+            1,119,10,123,1,123,10,127,2,127,10,131,1,5,
+            131,135,1,10,135,139,1,139,2,143,1,6,143,0,99,2,14,0,0)
+        val intCode = doOperation(miniCodes)
+        if (intCode[0] == 19690720) {
+            break@loop
+        }
+        if(verb < noun) {
+            verb++
+        }
+        else if (verb == noun){
+            noun++
+            verb=0
+        }
+    } while(true)
+    val answer = 100 * noun + verb
+    println("noun ${noun}")
+    println("verb ${verb}")
+    println("answer ${answer}")
+}
+
+fun main3() {
+    val noun = 12
+    val verb = 2
+    val miniCodes = arrayOf(1,noun,verb,3,1,1,2,3,1,3,4,3,1
         ,5,0,3,2,6,1,19,1,5,19,23,1,13,23,27,
         1,6,27,31,2,31,13,35,1,9,35,39,2,39,13
         ,43,1,43,10,47,1,47,13,51,2,13,51,55,1,55
@@ -39,4 +73,8 @@ fun main() {
         131,135,1,10,135,139,1,139,2,143,1,6,143,0,99,2,14,0,0)
     val actual = doOperation(miniCodes)
     println(actual[0])
+}
+
+fun main() {
+    findNounAndVerb()
 }
